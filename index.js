61ad90f2364a43253c97e108114f1728af575e3a
Colleen Murray <colleenkhm@gmail.com>
2022-04-07 06:30:27 -0500

    added full array of input objects

--- index.js
+++ index.js
@@ -14,12 +14,37 @@ const questions = [
         name: "description",
         message:"Description of project:"
     },
+    {
+        type: "input",
+        name: "table",
+        message: "Please provide links for the contents of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide instructions for installation"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please give a brief description of what this project is used for"
+    },
     {   
         type: "list",
         name: "license",
         message: "What license do you want to apply to your project?",
         choices: ["Apache", "MIT", "Mozilla", "None"]
-    }
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please list any contributors to this project"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please give a brief description of what this project is used for"
+    },
 ];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -31,6 +56,11 @@ function init() {
         // var markdown = generateMarkdown(answers) as alternative
     })
 }
+// example for function to create readme file - maybe needs to go in "generateMarkdown??"
+/* fs.writeFile('README.md', generatePage(name, github), err => {
+    if (err) throw err;
 
+    console.log('Portfolio complete! Check out index.html to see the output!')
+}); */
 // Function call to initialize app
 init();
